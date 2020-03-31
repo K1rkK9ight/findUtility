38aa4be239b3cf574e5916bc650bc56d82ca3ca7
K1rkK9ght <andrianov.vital@yandex.ru>
2020-03-31 18:37:13 +0300

    problem with console

---
+++ src/main/kotlin/Find/Find.kt
@@ -0,0 +1,41 @@
+package Main
+
+import org.kohsuke.args4j.Argument
+import org.kohsuke.args4j.CmdLineException
+import org.kohsuke.args4j.CmdLineParser
+import org.kohsuke.args4j.Option
+import java.io.*
+
+fun main(args: Array<String>) {
+    Find().correctControl(args)
+    Find().createPath()
+}
+
+class Find {
+
+    @Option(name = "-d", usage = "Поиск файла в текущей директории")
+    private val directory = File("")
+
+    @Option(name = "-r", usage = "Поиск файл во всех поддиректориях")
+    private val subDirectory = false
+
+    @Argument(required = true, usage = "Название нужного файла")
+    private val file = ""
+
+    fun correctControl(args: Array<String>) {
+        val parse = CmdLineParser(args)
+        try {
+            parse.parseArgument(*args)
+        } catch (Exception: CmdLineException) {
+            println("java -jar НазваниеJarФайлаПроектаFindUtility.jar -r -d Directory НазваниеФайла")
+        }
+    }
+
+    fun createPath() {
+        val foundList = mutableListOf<String>()
+        if (directory.isDirectory) {
+            for (path in RecursionFind.directoriesResearch(directory, file, subDirectory, foundList)) println(path)
+        }
+    }
+
+}
---
+++ src/test/kotlin/Find/FindTest.kt
@@ -0,0 +1,70 @@
+package Find
+
+import Main.RecursionFind
+import org.junit.Test
+import java.io.File
+import kotlin.test.assertEquals
+
+internal class FindTest {
+
+    @Test
+    fun test1() {
+        val directory = File("files4TestFind")
+        val file = "watchList.txt"
+        val subDirectory = true
+        val assertPath = mutableListOf("files4TestFind\\watchList.txt")
+        val foundFile = mutableListOf<String>()
+        assertEquals(assertPath, RecursionFind.directoriesResearch(directory, file, subDirectory, foundFile))
+    }
+
+    @Test
+    fun test2() {
+        val directory = File("src")
+        val file = "file4Test.txt"
+        val subDirectory = true
+        val assertPath = mutableListOf("src\\file4Test.txt")
+        val foundFile = mutableListOf<String>()
+        assertEquals(assertPath, RecursionFind.directoriesResearch(directory, file, subDirectory, foundFile))
+    }
+
+    @Test
+    fun test3() {
+        val directory = File("files4TestFind")
+        val file = "kursOVT.txt"
+        val subDirectory = true
+        val assertPath = mutableListOf("files4TestFind\\kursOVT.txt")
+        val foundFile = mutableListOf<String>()
+        assertEquals(assertPath, RecursionFind.directoriesResearch(directory, file, subDirectory, foundFile))
+    }
+
+    @Test
+    fun test4() {
+        val directory = File("files4TestFind")
+        val file = "=_=.txt"
+        val subDirectory = true
+        val assertPath = mutableListOf("files4TestFind\\=_=\\=_=.txt")
+        val foundFile = mutableListOf<String>()
+        assertEquals(assertPath, RecursionFind.directoriesResearch(directory, file, subDirectory, foundFile))
+    }
+
+    @Test
+    fun test5() {
+        val directory = File("src")
+        val file = "hidden.txt"
+        val subDirectory = true
+        val assertPath = mutableListOf("src\\recursionCheck\\1\\2\\3\\hidden.txt")
+        val foundFile = mutableListOf<String>()
+        assertEquals(assertPath, RecursionFind.directoriesResearch(directory, file, subDirectory, foundFile))
+    }
+
+    @Test
+    fun test6() {
+        val directory = File("src")
+        val file = "non.txt"
+        val subDirectory = false
+        val assertPath = mutableListOf("Такого файла не существует!")
+        val foundFile = mutableListOf<String>()
+        assertEquals(assertPath, RecursionFind.directoriesResearch(directory, file, subDirectory, foundFile))
+    }
+
+}
